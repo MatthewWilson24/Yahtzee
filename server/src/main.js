e7a21648dab834e9e193df534af4f3ab678449de
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-10-21 21:28:59 +0100

    Configured Express routing

--- server/src/main.js
+++ server/src/main.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { apiRouter } from './api/apiRouter.js'
 
 const app = express()
 const port = process.env.port || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const currentModuleFile = fileURLToPath(import.meta.url)
+const currentModuleDirectory = path.dirname(currentModuleFile)
+const clientDirectory = path.join(currentModuleDirectory, '../../client')
+
+app.use(express.static(clientDirectory))
+app.use('/api', apiRouter)
 
 app.listen(port, () => {
-  console.log(`Yahtzee app listening at http://localhost:${port}`)
+    console.log(`Yahtzee app listening at http://localhost:${port}`)
 })
