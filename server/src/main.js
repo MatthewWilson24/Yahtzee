c8789ab4eda61a82d4b9605e31fbc83dfc2386d7
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-10-21 20:04:03 +0100

    Added Express dependency and server setup skeleton

--- server/src/main.js
+++ server/src/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+import express from 'express'
+
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Yahtzee app listening at http://localhost:${port}`)
+})
