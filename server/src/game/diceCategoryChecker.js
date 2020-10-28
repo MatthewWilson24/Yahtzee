f98ee9cf7ec7ed1438cd83aa843e7057bb62ad3f
James Tan <jameshuatan@gmail.com>
2020-10-28 15:13:06 +0000

    Implement diceCategoryChecker.js

--- server/src/game/diceCategoryChecker.js
+++ server/src/game/diceCategoryChecker.js
@@ -1,26 +1,53 @@
 class DiceCategoryChecker {
+    countDiceValues(dice) {
+        let counts = [0, 0, 0, 0, 0, 0]
+        for (let die of dice) {
+            counts[die - 1] += 1
+        }
+        return counts
+    }
+
+    calculateDifferences(dice) {
+        dice = [...new Set(dice)]
+        dice.sort()
+        let differences = []
+        for (let i = 0; i < dice.length - 1; i += 1) {
+            differences.push(dice[i + 1] - dice[i])
+        }
+        return differences
+    }
+
     isThreeOfAKind(dice) {
-        
+        return Math.max(...this.countDiceValues(dice)) >= 3
     }
 
     isFourOfAKind(dice) {
-
+        return Math.max(...this.countDiceValues(dice)) >= 4
     }
 
     isFullHouse(dice) {
-
+        let values = this.countDiceValues(dice)
+        return (values.includes(3) && values.includes(2)) || values.includes(5)
     }
 
     isLowStraight(dice) {
+        let differences = this.calculateDifferences(dice)
 
+        // Check if [1, 1, 1] is a subarray of differences
+        return [1, 1, 1].every(
+            ((i) => (v) => (i = differences.indexOf(v, i) + 1))(0)
+        )
     }
 
     isHighStraight(dice) {
-
+        let differences = this.calculateDifferences(dice)
+        return (
+            differences.length === 4 && differences.every((diff) => diff == 1)
+        )
     }
 
     isYahtzee(dice) {
-
+        return Math.max(...this.countDiceValues(dice)) >= 5
     }
 }
 
--- server/test/game/diceCategoryChecker.spec.js
+++ server/test/game/diceCategoryChecker.spec.js
@@ -1,93 +1,93 @@
 import { diceCategoryChecker } from '../../src/game/diceCategoryChecker.js'
 
-describe.skip("diceCategoryChecker tests", () => {
-    describe("isThreeOfAKind", () => {
-        test("Returns true if three dice the same", () => {
+describe('diceCategoryChecker tests', () => {
+    describe('isThreeOfAKind', () => {
+        test('Returns true if three dice the same', () => {
             const dice = [1, 2, 1, 3, 1]
             expect(diceCategoryChecker.isThreeOfAKind(dice)).toBe(true)
         })
 
-        test("Returns true if more than three dice the same", () => {
+        test('Returns true if more than three dice the same', () => {
             const dice = [6, 6, 6, 6, 6]
             expect(diceCategoryChecker.isThreeOfAKind(dice)).toBe(true)
         })
 
-        test("Returns false if fewer than three dice the same", () => {
+        test('Returns false if fewer than three dice the same', () => {
             const dice = [1, 3, 1, 4, 2]
             expect(diceCategoryChecker.isThreeOfAKind(dice)).toBe(false)
         })
     })
 
-    describe("isFourOfAKind", () => {
-        test("Returns true if four dice the same", () => {
+    describe('isFourOfAKind', () => {
+        test('Returns true if four dice the same', () => {
             const dice = [1, 2, 1, 1, 1]
             expect(diceCategoryChecker.isFourOfAKind(dice)).toBe(true)
         })
 
-        test("Returns true if more than four dice the same", () => {
+        test('Returns true if more than four dice the same', () => {
             const dice = [6, 6, 6, 6, 6]
             expect(diceCategoryChecker.isFourOfAKind(dice)).toBe(true)
         })
 
-        test("Returns false if fewer than four dice the same", () => {
+        test('Returns false if fewer than four dice the same', () => {
             const dice = [1, 3, 1, 4, 1]
             expect(diceCategoryChecker.isFourOfAKind(dice)).toBe(false)
         })
     })
 
-    describe("isFullHouse", () => {
-        test("Returns true if dice contain a pair and triple of identical values", () => {
+    describe('isFullHouse', () => {
+        test('Returns true if dice contain a pair and triple of identical values', () => {
             const dice = [1, 2, 1, 2, 1]
             expect(diceCategoryChecker.isFullHouse(dice)).toBe(true)
         })
 
-        test("Returns true if all five dice have same value", () => {
+        test('Returns true if all five dice have same value', () => {
             const dice = [3, 3, 3, 3, 3]
             expect(diceCategoryChecker.isFullHouse(dice)).toBe(true)
         })
 
-        test("Returns false if dice do not contain a pair and triple of identical values", () => {
+        test('Returns false if dice do not contain a pair and triple of identical values', () => {
             const dice = [1, 1, 2, 2, 3]
             expect(diceCategoryChecker.isFullHouse(dice)).toBe(false)
         })
     })
 
-    describe("isLowStraight", () => {
-        test("Returns true if four dice appear in a sequence", () => {
+    describe('isLowStraight', () => {
+        test('Returns true if four dice appear in a sequence', () => {
             const dice = [4, 2, 3, 5, 2]
             expect(diceCategoryChecker.isLowStraight(dice)).toBe(true)
         })
 
-        test("Returns true if five dice appear in a sequence", () => {
+        test('Returns true if five dice appear in a sequence', () => {
             const dice = [3, 6, 2, 5, 4]
             expect(diceCategoryChecker.isLowStraight(dice)).toBe(true)
         })
 
-        test("Returns false if four dice do not appear in a sequence", () => {
+        test('Returns false if four dice do not appear in a sequence', () => {
             const dice = [3, 6, 3, 1, 2]
             expect(diceCategoryChecker.isLowStraight(dice)).toBe(false)
         })
     })
 
-    describe("isHighStraight", () => {
-        test("Returns true if five dice appear in a sequence", () => {
+    describe('isHighStraight', () => {
+        test('Returns true if five dice appear in a sequence', () => {
             const dice = [4, 2, 3, 5, 6]
             expect(diceCategoryChecker.isHighStraight(dice)).toBe(true)
         })
 
-        test("Returns false if five dice do not appear in a sequence", () => {
+        test('Returns false if five dice do not appear in a sequence', () => {
             const dice = [4, 6, 3, 1, 2]
             expect(diceCategoryChecker.isHighStraight(dice)).toBe(false)
         })
     })
 
-    describe("isYahtzee", () => {
-        test("Returns true if all five dice are the same", () => {
+    describe('isYahtzee', () => {
+        test('Returns true if all five dice are the same', () => {
             const dice = [4, 4, 4, 4, 4]
             expect(diceCategoryChecker.isYahtzee(dice)).toBe(true)
         })
 
-        test("Returns false if all five dice are not the same", () => {
+        test('Returns false if all five dice are not the same', () => {
             const dice = [4, 4, 3, 4, 4]
             expect(diceCategoryChecker.isYahtzee(dice)).toBe(false)
         })
