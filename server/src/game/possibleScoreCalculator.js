bc0a55d54896ff9b3703a9c812a8c5a144999630
Heemy <heemy.k@gmail.com>
2020-10-29 16:00:19 +0000

    Wrote code for store.js and main.js

--- server/src/game/possibleScoreCalculator.js
+++ server/src/game/possibleScoreCalculator.js
@@ -1,7 +1,19 @@
 import { diceScoreCalculator } from './diceScoreCalculator.js'
+import { scoringCategories } from './categories.js'
 
 class PossibleScoreCalculator {
     allPossibleScores(scorecard, dice) {
+        // let diceCatChecker = {}
+        // for (category of scoringCategories){
+        //     diceCatChecker[key] = value
+            
+        // }
+        // let diceCatChecker = {}
+        // diceCatChecker.key(DiceCategoryChecker.isThreeOfAKind(dice))
+
+
+
+        
         
     }
 }
--- server/src/store/store.js
+++ server/src/store/store.js
@@ -3,15 +3,34 @@ export class Store {
         this.contents = {}
     }
 
-    add(key, value) {
+    /*
+        const obj = {
+            prop: val
+        }
 
+        obj.prop -> val
+        obj["prop"] -> val
+    */
+    /*
+        const store = new Store()
+        store.add('myKey', 10)
+        store.get('myKey') -> 10
+        store.remove('myKey')
+    */
+
+    add(key, value) {
+        this.contents["key"] = value
+        return this.contents  
     }
 
     remove(key) {
+        // const storeRemove = {key}
+        // this.store.contents = 
+        delete this.contents["key"]
 
     }
 
     get(key) {
-
+        return this.contents["key"]
     }
 }
